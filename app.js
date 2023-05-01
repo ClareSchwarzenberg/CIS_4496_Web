76b05f1b390cb8dacaac8a29458f76133ddbd684
ClareSchwarzenberg <116851408+ClareSchwarzenberg@users.noreply.github.com>
2023-05-01 05:26:33 +0000

    Edit files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-d3.csv("movies.csv").then(function (data) {
+d3.csv("tf_predictions.csv").then(function (data) {
   // console.log(data);
 
-  var movies = data;
+  var pred = data;
 
   var button = d3.select("#button");
 
@@ -12,30 +12,17 @@ d3.csv("movies.csv").then(function (data) {
 
   function runEnter() {
     d3.select("tbody").html("")
-    d3.selectAll("p").classed('noresults', true).html("")
+    //d3.selectAll("p").classed('noresults', true).html("")
     d3.event.preventDefault();
     var inputElement = d3.select("#user-input");
-    var inputValue = inputElement.property("value").toLowerCase().trim();
+    //var inputValue = inputElement.property("value").toLowerCase().trim();
+    var inputValue = inputElement.property("value");
 
     // console.log(inputValue.length);
     // console.log(movies);
-    if (inputValue.length < 6){
-      d3.select("p").classed('noresults2', true).html("<center><strong>Please try using more than 5 characters to avoid too many results!</strong>")
-      inputValue = "Something to give no results"
-    }
-    var filteredData = movies.filter(movies => movies.actors.toLowerCase().trim().includes(inputValue));
+    var filteredData = pred.filter(pred => pred.article_id.includes(inputValue));
     // console.log(filteredData.length)
-    if (filteredData.length === 0 && inputValue !== "Something to give no results"){
-      d3.select("p").classed('noresults', true).html("<center><strong>No results. Please check your spelling!</strong>")
-    }
-    output = _.sortBy(filteredData, 'avg_vote').reverse()
-
-    for (var i = 0; i < filteredData.length; i++) {
-      // console.log(output[i]['original_title'])
-      // console.log(output[i]['avg_vote'])
-      // d3.select("tbody>tr>td").text(output[i]['original_title']);
-      d3.select("tbody").insert("tr").html("<td>"+[i+1]+"</td>"+"<td>"+"<a href=https://www.imdb.com/title/"+output[i]['imdb_title_id']+" target='_blank'>"+(output[i]['original_title'])+"</a>"
-      + "</td>" +"<td>" +(output[i]['avg_vote'])+"</td>" +"<td>" +(output[i]['year'])+"</td>"  +"<td>" +(output[i]['director'])+"</td>"+"<td>" +(output[i]['description'])+"</td>") }
+    d3.select("tbody").insert("tr").html(inputValue)
   };
   window.resizeTo(screen.width,screen.height)
 
